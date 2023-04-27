e39380db564c0a10cd15e0c245b8b3e475288a2c
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-04-27 20:23:20 +0100

    feat: made atom work with mutability

--- src/atom.rs
+++ src/atom.rs
@@ -1,19 +1,28 @@
-pub struct Atom<Undo: FnOnce()> {
+pub struct Atom<T, R, Act: FnOnce(&mut T) -> R, Undo: FnOnce(&mut T)> {
+    val: T,
+    act: Option<Act>,
     undo: Option<Undo>,
 }
 
-impl<Undo: FnOnce()> Atom<Undo> {
-    pub fn new(undo: Undo) -> Self {
-        Self { undo: Some(undo) }
+impl<T, R, Act: FnOnce(&mut T) -> R, Undo: FnOnce(&mut T)> Atom<T, R, Act, Undo> {
+    pub fn new(val: T, act: Act, undo: Undo) -> Self {
+        Self {
+            val: val,
+            act: Some(act),
+            undo: Some(undo),
+        }
+    }
+    pub fn eval(mut self) -> R {
+        self.act.take().unwrap()(&mut self.val)
     }
     pub fn cancel(mut self) {
         self.undo.take();
     }
 }
-impl<Undo: FnOnce()> Drop for Atom<Undo> {
+impl<T, R, Act: FnOnce(&mut T) -> R, Undo: FnOnce(&mut T)> Drop for Atom<T, R, Act, Undo> {
     fn drop(&mut self) {
         if let Some(undo) = self.undo.take() {
-            undo();
+            undo(&mut self.val);
         }
     }
 }
@@ -26,9 +35,15 @@ mod tests {
     fn atom_runs_on_drop_if_uncancelled() {
         let mut scoped = 12;
         {
-            let atom = Atom::new(|| {
-                scoped = 0;
-            });
+            let atom = Atom::new(
+                &mut scoped,
+                |v| {
+                    **v += 2;
+                },
+                |v| {
+                    **v = 0;
+                },
+            );
             drop(atom);
         }
         assert_eq!(scoped, 0);
@@ -38,9 +53,15 @@ mod tests {
     fn cancelling_atom_stops_it_running_on_drop() {
         let mut scoped = 12;
         {
-            let atom = Atom::new(|| {
-                scoped = 0;
-            });
+            let atom = Atom::new(
+                &mut scoped,
+                |s| {
+                    **s = 13;
+                },
+                |s| {
+                    **s = 0;
+                },
+            );
             atom.cancel();
         }
         assert_eq!(scoped, 12);
